454d9a374253c113affab5db4ce1d50b2056235d
kushu30 <kushu123456789@gmail.com>
2026-07-07 04:38:47 +0530

    feat: support Streamlit Cloud secrets for deployment

--- app.py
+++ app.py
@@ -1,4 +1,9 @@
+import os
+
 import streamlit as st
+
+if "GROQ_API_KEY" in st.secrets:
+    os.environ["GROQ_API_KEY"] = st.secrets["GROQ_API_KEY"]
 
 from evaluation.judge import LlmJudgeMetric
 from evaluation.metrics import BertScoreMetric, BleuMetric, RougeLMetric, WeightedEvaluator
